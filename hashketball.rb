c84660e8433a19e9969b828edbe403ee72f27795
Blake Rose <bdecktor@gmail.com>
2019-05-13 00:37:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,23 @@
 # Write your code here!
+game_hash = {
+  :home => {
+    :team_name => team_name_str
+    :colors => []
+    :players => {
+      :number
+      :shoe
+      :points
+      :rebounds
+      :assists
+      :steals
+      :blocks
+      :slam_dunks
+    }
+  }
 
+  :away => {
 
+  }
+}
 
-
-
-
-
-
-
-
+def game_hash
