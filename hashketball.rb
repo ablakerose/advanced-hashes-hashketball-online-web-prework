d35eb71fe8bedb2940c1bdb8694e5df135f64938
Blake Rose <bdecktor@gmail.com>
2019-05-13 01:21:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ def num_points_scored(player_name, game)
     team_hash[:players].each do |player, player_hash|
       if player_hash[:name] == player_name
         return player_hash[:points]
-      end
+      
     end
   end 
 end
